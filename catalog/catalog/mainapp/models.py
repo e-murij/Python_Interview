87dd1015552b17066e328083c5e0e77381a6de4f
e.murij <murij.kat@gmail.com>
2022-05-30 21:28:20 +0700

    add catalog

---
+++ catalog/catalog/mainapp/models.py
@@ -0,0 +1,62 @@
+from django.db import models
+
+
+class Provider(models.Model):
+    name = models.CharField(
+        verbose_name='название',
+        max_length=128,
+    )
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        verbose_name = "поставщик"
+        verbose_name_plural = "поставщики"
+
+
+class Measure(models.Model):
+    name = models.CharField(
+        verbose_name='единица измерения',
+        max_length=128,
+    )
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        verbose_name = "единица измерения"
+        verbose_name_plural = "единицы измерения"
+
+
+class Catalog(models.Model):
+    name = models.CharField(
+        verbose_name='название',
+        max_length=128,
+    )
+    price = models.DecimalField(
+        verbose_name='цена',
+        max_digits=8,
+        decimal_places=2,
+        default=0,
+    )
+    add_time = models.DateField(
+        verbose_name='дата поступления',
+    )
+    measure = models.ForeignKey(
+        Measure,
+        null=True,
+        on_delete=models.SET_NULL,
+        verbose_name='единица измерения',
+    )
+    provider = models.ForeignKey(
+        Provider,
+        null=True,
+        on_delete=models.SET_NULL,
+        verbose_name='поставщик',
+    )
+
+    class Meta:
+        verbose_name = "товар"
+        verbose_name_plural = "товары"
+
